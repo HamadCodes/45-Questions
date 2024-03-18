990678a0e9dcdc9741d7ebaa284c3abeeb7ae7ec
Hamad <155316963+HamadCodes@users.noreply.github.com>
2024-03-19 03:30:10 +0500

    Update script.ts

--- 45/script.ts
+++ 45/script.ts
@@ -1,14 +1,15 @@
-function createCar(manufacturer, model, color, year) {
+function createCar(manufacturer, model, color, year,...more) {
     let car = {
         manufacturer: manufacturer,
         model: model,
         color:color,
         year: year
     }
+    more.forEach(([a,b]) => car[a]=b)
 
     return car;
 }
 
-let myCar = createCar('Toyota', 'Camry', 'blue', 2023);
+let myCar = createCar('Toyota', 'Camry', 'blue', 2023,["Speed","100km/h"],["RearCamera",true]);
 
 console.log(myCar);
